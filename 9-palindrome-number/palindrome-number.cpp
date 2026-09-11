c1dbb276cc6e9367388a298a5381ca358284b81d
Mahfooj Alam <contacttomaroof@gmail.com>
2026-09-11 18:28:12 +0530

    Time: 3 ms (43.84%) | Memory: 8.6 MB (35.92%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        
+        int reverted = 0;
+        while (x > reverted) {
+            reverted = reverted * 10 + x % 10;
+            x /= 10;
+        }
+        
+        // For even digit count: x == reverted
+        // For odd digit count: middle digit is dropped, so x == reverted / 10
+        return x == reverted || x == reverted / 10;
+    }
+};
