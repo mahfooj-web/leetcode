03f9234dc0cdbed9518a8d0dc178d25b8d82d084
Mahfooj Alam <contacttomaroof@gmail.com>
2026-09-13 23:30:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (26.13%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            // Prune: smallest possible sum from i is too big
+            if ((long long)nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) break;
+            // Prune: largest possible sum from i is too small
+            if ((long long)nums[i] + nums[n-1] + nums[n-2] + nums[n-3] < target) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                if ((long long)nums[i] + nums[j] + nums[j+1] + nums[j+2] > target) break;
+                if ((long long)nums[i] + nums[j] + nums[n-1] + nums[n-2] < target) continue;
+
+                int left = j + 1, right = n - 1;
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+};
